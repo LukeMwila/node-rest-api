eb5e5188ae2a72333eb6bf0efe5580c7d107f0c5
Luke Mwila <outlier.developer@gmail.com>
2018-02-24 14:31:54 +0200

    Added routes

---
+++ api/routes/orders.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const router = express.Router()
+
+/**
+ * Handling GET requests
+ */
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Orders were fetched'
+    })
+})
+
+/**
+ * Handling POST requests
+ */
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Order was created'
+    })
+})
+
+/**
+ * GET a single order
+ */
+router.get('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Order details',
+        orderId: req.params.orderId
+    })
+})
+
+/**
+ * DELETE request to remove/delete a order
+ */
+router.delete('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted order',
+        orderId: req.params.orderId
+    })
+})
+
+module.exports = router
---
+++ api/routes/products.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const router = express.Router()
+
+/**
+ * Handling GET requests
+ */
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Products were fetched'
+    })
+})
+
+/**
+ * Handling POST requests
+ */
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Product was created'
+    })
+})
+
+/**
+ * GET a single product
+ */
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId
+    res.status(200).json({
+        message: 'Product was fetched'
+    })
+})
+
+/**
+ * PATCH request used toupdate product
+ */
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product'
+    })
+})
+
+/**
+ * DELETE request to remove/delete a product
+ */
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted product'
+    })
+})
+
+module.exports = router
