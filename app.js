36fe41df87292f715d7d644797fca9927fd4c014
Luke Mwila <outlier.developer@gmail.com>
2018-03-01 23:43:48 +0200

    Updated connection string again.

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
 
 mongoose.connect(
-    'mongodb connection string goes here',
-    {
-        useMongoClient: true
-    }
+  'mongodb://nodeshop:'+ process.env.MONGO_ATLAS_PW +'@node-rest-shop-shard-00-00-qomy8.mongodb.net:27017,node-rest-shop-shard-00-01-qomy8.mongodb.net:27017,node-rest-shop-shard-00-02-qomy8.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin',
+  {
+    useMongoClient: true
+  }
 )
 
 app.use(morgan('dev'))
