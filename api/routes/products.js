a62c2b87e7926ce9bf4424b26e28f18b7ee83fc2
Luke Mwila <outlier.developer@gmail.com>
2018-03-12 23:16:37 +0200

    Added multer to handle image file uploads

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,6 +2,31 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const Product = require('../models/products')
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, './uploads/')
+    },
+    filename: function(req, file, cb){
+        cb(null, new Date().toISOString() + file.originalname)
+    }
+})
+
+const fileFilter = (req, file, cb) => {
+    // reject a file
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+        cb(null, true)
+    }else{
+        cb(null, false)
+    }
+}
+
+const upload = multer({storage: storage, 
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+})
 
 /**
  * Handling GET requests
@@ -10,8 +35,13 @@ router.get('/', (req, res, next) => {
     Product.find()
     .exec()
     .then(docs => {
-        console.log(docs)
-        res.status(200).json(docs)
+        const response = {
+            count: docs.length,
+            products: docs.map(doc => {
+                return doc
+            })
+        }
+        res.status(200).json(response)
     })
     .catch(err => {
         console.log(err)
@@ -24,23 +54,26 @@ router.get('/', (req, res, next) => {
 /**
  * Handling POST requests
  */
-router.post('/', (req, res, next) => {
+router.post('/', upload.single('productImage'), (req, res, next) => {
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
-        price: req.body.price
+        price: req.body.price,
+        productImage: req.file.path
     })
     product
     .save()
     .then(result => {
-        console.log(result)
         res.status(201).json({
         message: 'Product was created',
-        createdProduct: result
+        createdProduct: {
+            name: result.name,
+            price: result.price,
+            _id: result._id
+        }
     })
     })
     .catch(err => {
-        console.log(err)
         res.status(500).json({
             error: err
         })
@@ -55,7 +88,6 @@ router.get('/:productId', (req, res, next) => {
     Product.findById(id)
     .exec()
     .then(doc => {
-        console.log(doc)
         if(doc){
             res.status(200).json(doc)
         }else{
@@ -64,7 +96,6 @@ router.get('/:productId', (req, res, next) => {
         
     })
     .catch(err => {
-        console.log(err)
         res.status(500).json({error: err})
     })
 })
@@ -81,11 +112,11 @@ router.patch('/:productId', (req, res, next) => {
     Product.update({ _id: id }, { $set: updateOps })
     .exec()
     .then(result => {
-        console.log(result)
-        res.status(200).json(result)
+        res.status(200).json({
+            message: 'Product updated'
+        })
     })
     .catch(err => {
-        console.log(err)
         res.status(500).json({
             error: err
         })
@@ -102,7 +133,9 @@ router.delete('/:productId', (req, res, next) => {
     })
     .exec()
     .then(result => {
-        res.status(200).json(result)
+        res.status(200).json({
+            message: 'Product deleted'
+        })
     })
     .catch(err => {
         console.log(err)
